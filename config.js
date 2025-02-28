f86835d53869d2a0e469c1b1488527ed927e096a
ChamiOFC <chamiofc92@gmail.com>
2025-02-28 14:57:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Past Your Session Id 🍃",
+SESSION_ID: process.env.SESSION_ID || "𝐐𝐔𝐄𝐄𝐍-𝐑𝐀𝐒𝐇𝐔-𝐌𝐃=Z0pzWTxC#oKU7p-YoWTQKZM89Las_nxUpZfhbGFn4snjYHPm0W94",
 //උඩ ☝️ තියෙන Past Sesion Id කියන එක අයින් කරලා ඔයාගෙ Sesion Id එක Past කරන්න 😁
 MONGODB: process.env.MONGODB || "Past Your MongoDB Url 🍃",
 //උඩ ☝️ තියෙන Past MongoDB Url කියන එක අයින් කරලා ඔයාගෙ MongoDB Url එක Past කරන්න 😁
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
-BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
-LANG: process.env.BOT_LANG || 'EN' ,
+BOT_NAME: process.env.BOT_NAME || "𝗗αʀᴋ_𝗤ᴜᴇᴇɴ-𝗠𝗗",
+LANG: process.env.BOT_LANG || 'SI' ,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 };
